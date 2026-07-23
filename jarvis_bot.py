0d94cfc2164f94a68510638191d7e1dbe97e5840
ppaapaaa34-hub <ppaapaaa34@gmail.com>
2026-07-23 21:07:01 +0300

    Update jarvis_bot.py

--- jarvis_bot.py
+++ jarvis_bot.py
@@ -134,7 +134,7 @@ safety_settings = [
 ]
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="gemini-2.5-flash",
     generation_config=generation_config,
     safety_settings=safety_settings,
     system_instruction=(
