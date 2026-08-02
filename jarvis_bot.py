22334b56a65e88bf2c230afcdcc0e36cfce118fe
ppaapaaa34-hub <ppaapaaa34@gmail.com>
2026-08-02 16:51:28 +0300

    Update jarvis_bot.py

--- jarvis_bot.py
+++ jarvis_bot.py
@@ -130,7 +130,7 @@ safety_settings = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
 ]
 
-model = genai.GenerativeModel(
+#model = genai.GenerativeModel(
     model_name="gemini-2.5-flash",
     generation_config=generation_config,
     safety_settings=safety_settings,
