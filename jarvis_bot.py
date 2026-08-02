bbded9a444b6b721c1b49b59cdc7fa6acd0fb8fd
ppaapaaa34-hub <ppaapaaa34@gmail.com>
2026-08-02 16:54:12 +0300

    Update jarvis_bot.py

--- jarvis_bot.py
+++ jarvis_bot.py
@@ -11,6 +11,7 @@ import edge_tts
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import telebot
 from telebot import types
+import google.generativeai as genai
 from PIL import Image
 import psycopg2
 import discord
@@ -130,7 +131,7 @@ safety_settings = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
 ]
 
-#model = genai.GenerativeModel(
+model = genai.GenerativeModel(
     model_name="gemini-2.5-flash",
     generation_config=generation_config,
     safety_settings=safety_settings,
